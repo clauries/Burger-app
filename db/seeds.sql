632b43b19fb509838907533ec3d71f06a2d9003d
clauries <55369403+clauries@users.noreply.github.com>
2020-02-01 09:52:09 -0800

    Created file system, server, and db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Whiskey River BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
